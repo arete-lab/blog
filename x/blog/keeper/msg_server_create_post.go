94f5dc478efbe8272b64052435d919b340da52fc
Francesco De Falco <defalco.fnc@gmail.com>
2023-06-03 18:34:31 +0200

    Handling the "create post" message

--- x/blog/keeper/msg_server_create_post.go
+++ x/blog/keeper/msg_server_create_post.go
@@ -4,14 +4,22 @@ import (
 	"context"
 
 	"blog/x/blog/types"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k msgServer) CreatePost(goCtx context.Context, msg *types.MsgCreatePost) (*types.MsgCreatePostResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
-
-	// TODO: Handling the message
-	_ = ctx
-
-	return &types.MsgCreatePostResponse{}, nil
+	var post = types.Post{
+		Creator: msg.Creator,
+		Title:   msg.Title,
+		Body:    msg.Body,
+	}
+	id := k.AppendPost(
+		ctx,
+		post,
+	)
+	return &types.MsgCreatePostResponse{
+		Id: id,
+	}, nil
 }
