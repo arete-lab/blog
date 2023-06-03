00582718b3bcfb93a465d2b175a3b40b3bdb9d83
Francesco De Falco <defalco.fnc@gmail.com>
2023-06-03 18:44:28 +0200

    Delete a message (completed)

--- x/blog/keeper/msg_server_delete_post.go
+++ x/blog/keeper/msg_server_delete_post.go
@@ -2,16 +2,23 @@ package keeper
 
 import (
 	"context"
+	"fmt"
 
 	"blog/x/blog/types"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) DeletePost(goCtx context.Context, msg *types.MsgDeletePost) (*types.MsgDeletePostResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
-
-	// TODO: Handling the message
-	_ = ctx
-
+	val, found := k.GetPost(ctx, msg.Id)
+	if !found {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("key %d doesn't exist", msg.Id))
+	}
+	if msg.Creator != val.Creator {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "incorrect owner")
+	}
+	k.RemovePost(ctx, msg.Id)
 	return &types.MsgDeletePostResponse{}, nil
 }
